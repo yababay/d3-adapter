76ebedac3e5b243216c6afcf8826be800a5084ed
Михаил Беляков <bigbelk@live.ru>
2021-12-22 01:29:55 +0300

    Linear- and bar- charts.

---
+++ index.js
@@ -0,0 +1,7 @@
+import D3Scales from './src/D3Scales.js'
+import D3SvgCanvas from './src/D3SvgCanvas.js';
+import D3LinearChart from './src/D3LinearChart.js';
+import D3BarChart from './src/D3BarChart.js';
+
+export { D3SvgCanvas, D3Scales, D3LinearChart, D3BarChart };
+
